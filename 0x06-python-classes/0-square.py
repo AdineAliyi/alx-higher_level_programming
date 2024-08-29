c242305a09bb02f8e9bb9b338d4185f667429d20
AdineAliyi <adinewalisaid@gmail.com>
2024-08-29 13:35:18 -0700

    An empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# 0-square.py
-"""class Square"""
 
 class Square:
-    """Defined empty class Square."""
     pass
--- 0x06-python-classes/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# 0-square.py
-"""A class Square"""
-
-
-class Square:
-    """A defined empty class Square."""
-    pass

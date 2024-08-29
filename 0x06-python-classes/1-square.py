847364728074c4c68818c31c17a832034c90909f
AdineAliyi <adinewalisaid@gmail.com>
2024-08-29 13:48:09 -0700

    A class based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defined Square class"""
+
+
+class Square:
+    """To Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a square
+        Args:
+            size (int): size of a side of the square
+        """
+        self.__size = size
